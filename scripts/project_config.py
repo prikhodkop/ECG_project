147034d62ad25f9b0d6f20f91d397872c33d6762
IvanPanin <panin@phystech.edu>
2015-12-16 00:51:53 +0300

    Added project configuration script and logging

---
+++ scripts/project_config.py
@@ -0,0 +1,9 @@
+"""
+This file contains basic settings of the project
+  * data location (RR, dta, raw, sample)
+"""
+
+path_to_dta = '../../data/dta/'
+path_to_RR = '../../data/RR/'
+path_to_raw = '../../data/raw/'
+path_to_sample = '../../data/sample/' # samples generated from initial data
